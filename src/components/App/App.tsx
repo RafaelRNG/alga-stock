0707ab85421f4843bfac7fd5b7287e3801d58e4f
RafaelRNG <rafael.gomila@hotmail.com>
2021-03-06 04:01:48 -0300

    Resetando form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,10 @@
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Header from '../Header';
-import Products from "../../shared/Table/Table.mockdata";
-import ProductForm from '../Products/ProductForm';
+import Products, { Product } from "../../shared/Table/Table.mockdata";
+import ProductForm, { ProductCreator } from '../Products/ProductForm';
 import './App.css';
+import { useState } from 'react';
 
 const headers: TableHeader[] = [
   { key: "id", value: "#" },
@@ -14,6 +15,25 @@ const headers: TableHeader[] = [
 
 function App() {
 
+  const [products, setProducts] = useState(Products);
+  const [updatingProduct, setUpdatingProduct] = useState<Product | undefined>(products[0]);
+
+  const handleProductSubmit = (product: ProductCreator) => {
+    setProducts([
+      ...products,
+      {
+        id: products.length + 1,
+        ...product
+      }
+    ])
+  }
+
+  const handleProductUpdate = (newProduct: Product) => {
+    setProducts(products.map(product => product.id === newProduct.id ? newProduct : product))
+
+    setUpdatingProduct(undefined);
+  }
+
   return (
     <div>
       <Header
@@ -22,10 +42,12 @@ function App() {
       <Container>
         <Table
           headers={headers}
-          data={Products} />
+          data={products} />
 
         <ProductForm
-        />
+          form={updatingProduct}
+          onSubmit={handleProductSubmit}
+          onUpdate={handleProductUpdate} />
       </Container>
     </div>
   );
--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -1,21 +1,49 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FC, useEffect } from "react";
 import Button from "../../shared/Button";
 import Form from "../../shared/Form"
 import Input from "../../shared/Input";
+import { Product } from "../../shared/Table/Table.mockdata";
 
-interface InitialState {
+export interface ProductCreator {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+interface InitialFormStates {
+  id?: number;
   name: string;
   price: string;
   stock: string;
 }
 
-const ProductForm = () => {
 
-  const [form, setForm] = useState<InitialState>({
-    name: "",
-    price: "",
-    stock: ""
-  });
+interface ProductFormProps {
+  form?: Product
+  onSubmit?: (product: ProductCreator) => void;
+  onUpdate?: (product: Product) => void;
+  onDelete?: (product: Product) => void;
+}
+
+const ProductForm: FC<ProductFormProps> = (props) => {
+  const initialFormState: InitialFormStates = props.form
+    ? {
+      id: props.form.id,
+      name: props.form.name,
+      price: String(props.form.price),
+      stock: String(props.form.stock)
+    }
+    : {
+      name: "",
+      price: "",
+      stock: ""
+    }
+
+  const [form, setForm] = useState<InitialFormStates>(initialFormState);
+
+  useEffect(() => {
+    setForm(initialFormState);
+  }, [props.form])
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = event.target;
@@ -26,36 +54,68 @@ const ProductForm = () => {
     })
   }
 
+  const updateProduct = (product: InitialFormStates) => {
+    const productDto = {
+      id: Number(product.id),
+      name: String(product.name),
+      price: parseFloat(product.price),
+      stock: Number(product.stock)
+    }
+
+    props.onUpdate && props.onUpdate(productDto);
+  }
+
+  const createProduct = (product: InitialFormStates) => {
+    const productDto = {
+      name: String(product.name),
+      price: parseFloat(product.price),
+      stock: Number(product.stock)
+    }
+
+    props.onSubmit && props.onSubmit(productDto);
+  }
+
+  const handleFormSubmit = () => {
+    form.id
+      ? updateProduct(form)
+      : createProduct(form)
+
+    setForm(initialFormState);
+  }
+
   return (
     <Form
       title="Product Form"
-      onSubmit={() => console.log(form)}>
+      onSubmit={handleFormSubmit}>
 
       <Input
         onChange={handleInputChange}
         name="name"
         label="Name"
         placeholder="E.g.: Cookie"
-        required />
+        required
+        value={form.name} />
 
       <Input
         onChange={handleInputChange}
         name="price"
         label="Price"
-        placeholder="E.g.: 1.25"
         step="0.01"
         min="0"
         type="number"
-        required />
+        placeholder="E.g.: 1.25"
+        required
+        value={form.price} />
 
       <Input
         onChange={handleInputChange}
         name="stock"
         label="Stock"
-        placeholder="E.g.: 15"
         min="0"
         type="number"
-        required />
+        placeholder="E.g.: 15"
+        required
+        value={form.stock} />
 
       <Button>Submit</Button>
     </Form>
