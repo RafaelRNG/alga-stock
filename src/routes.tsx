571b14b95567e2fe1dba3044881087220d418bc4
RafaelRNG <rafael.gomila@hotmail.com>
2021-03-10 13:27:05 -0300

    Criando a rota 404

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,16 @@ import {
    Route
 } from "react-router-dom";
 import HomeView from "./views/HomeView";
+import NotFoundView from "./views/NotFoundView";
 
 const Routes = () => {
    return (
       <BrowserRouter>
          <Switch>
             <Route exact path="/" component={HomeView} />
+
+
+            <Route path="*" component={NotFoundView} />
          </Switch>
       </BrowserRouter>
    )
